e4fc84f99f1c64e0849cf5e7d3890147a38982d6
thekirankumardash <thekirankumardash@gmail.com>
2016-01-04 18:25:39 +0530

    4.4 - Placing the header image behind the site title

--- header.php
+++ header.php
@@ -22,17 +22,24 @@
 
 <body <?php body_class(); ?>>
 
+<?php 
+if ( get_header_image() && !('blank' == get_header_textcolor()) ) { ?>
+    
 <header id="masthead" class="site-header" role="banner">
+	
+<?php } else { ?>
+   
+<header id="masthead" class="site-header masthead-fullscreen" role="banner">       
+ 
+<?php } ?>
     
-	<?php if ( get_header_image() ) : ?>
-    <div class="header-image">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-            <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-        </a>
-    </div>
-    <?php endif; // End header image check. ?>
-    
-    <div class="site-branding">
+    <?php 
+        if ( is_front_page() && is_home() &&  get_header_image() && !('blank' == get_header_textcolor()) ) { 
+            echo '<div class="site-branding header-background-image" style="background-image: url(' . get_header_image() . ')">'; 
+        } else {
+            echo '<div class="site-branding">';
+        }
+    ?>
         <div class="branding-box">
             <div class="title-wrapper">
             <?php
