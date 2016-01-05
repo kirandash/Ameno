be6a8335d93a658c5d29799d1ad0636713d89867
thekirankumardash <thekirankumardash@gmail.com>
2016-01-05 15:59:29 +0530

    Edit site description from p to h1.

--- header.php
+++ header.php
@@ -60,7 +60,7 @@ if ( get_header_image() && !('blank' == get_header_textcolor()) ) { ?>
             if ( is_front_page() && is_home() ) : ?>
                 <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
             <?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
             <?php
             endif;
     
