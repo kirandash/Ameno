369ec9af2be3b5921a2ac1b3725c49996e403b9c
thekirankumardash <thekirankumardash@gmail.com>
2016-01-06 10:24:35 +0530

    Re-edit 8.2 - Changing the output of meta elements

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,12 +25,12 @@ function k_ameno_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Published on %s', 'post date', 'k-ameno' ),
+		esc_html_x( '%s', 'post date', 'k-ameno' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'Author: %s', 'post author', 'k-ameno' ),
+		esc_html_x( '%s', 'post author', 'k-ameno' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -18,7 +18,7 @@
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( esc_html__( ', ', 'k-ameno' ) );
 			if ( $categories_list && k_ameno_categorized_blog() ) {
-				printf( '<div class="category-list"><span class="cat-links">' . esc_html__( 'Posted in %1$s', 'k-ameno' ) . '</span>', $categories_list . '</div>'); // WPCS: XSS OK.
+				printf( '<div class="category-list"><span class="cat-links">' . esc_html__( '%1$s', 'k-ameno' ) . '</span>', $categories_list . '</div>'); // WPCS: XSS OK.
 			}
 			
 				the_title( '<h1 class="entry-title">', '</h1>' );
