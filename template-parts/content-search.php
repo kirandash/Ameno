3df811c65883e5542c202b6368adeb580677cd6b
thekirankumardash <thekirankumardash@gmail.com>
2016-01-08 10:28:32 +0530

    11.12 - Customizing the search results and 404 templates.

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,21 +10,31 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php k_ameno_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php k_ameno_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<div class="index-box">
+        <header class="entry-header">
+            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    
+            <?php if ( 'post' === get_post_type() ) : ?>
+            <div class="entry-meta">
+				  <?php k_ameno_posted_on(); ?>
+                  <?php 
+                  if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) { 
+                      echo '<span class="comments-link">';
+                      comments_popup_link( __( 'Leave a comment', 'k-ameno' ), __( '1 Comment', 'k-ameno' ), __( '% Comments', 'k-ameno' ) );
+                      echo '</span>';
+                  }
+                  ?>
+                  <?php edit_post_link( __( ' | Edit', 'k-ameno' ), '<span class="edit-link">', '</span>' ); ?>
+            </div><!-- .entry-meta -->
+            <?php endif; ?>
+        </header><!-- .entry-header -->
+    
+        <div class="entry-summary">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-summary -->
+    
+        <footer class="entry-footer read-more">
+            <?php echo '<a href="' . get_permalink() . '" title="' . __('Read more ', 'k-ameno') . get_the_title() . '" rel="bookmark">Read more</a>'; ?>
+        </footer><!-- .entry-footer -->
+    </div>
 </article><!-- #post-## -->
